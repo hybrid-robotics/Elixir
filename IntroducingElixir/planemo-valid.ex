ac4eaf8ad1f54330f7057bf8b4702fa1d40bf974
Dale Weber <dalew@hybotics.com>
2015-12-11 21:26:08 -0800

    Removing old files

--- IntroducingElixir/planemo-valid.ex
+++
@@ -1,9 +0,0 @@
-defmodule Planemo do
-@moduledoc """
-Dale Weber <hybotics@skynet.run>
-
-From *Introducing Elixir*, O'Reilly Media, Inc., 2014.
-Copyright 2014 by Simon St.Laurent and J. David Eisenberg.
-"""
-
-defstruct name: :nil, gravity: 0, diameter: 0, distance_from_sun: 0
