e7e6c88a5027924ec6802c64836aed5f52e83cfc
Dale Weber <hybotics@hybotics.org>
2017-04-20 12:13:53 -0700

    Updated to add the /hello route.

--- Programming_Phoenix/hello/web/router.ex
+++ Programming_Phoenix/hello/web/router.ex
@@ -1,26 +1,29 @@
 defmodule Hello.Router do
-  use Hello.Web, :router
+	use Hello.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
+	pipeline :browser do
+		plug :accepts, ["html"]
+		plug :fetch_session
+		plug :fetch_flash
+		plug :protect_from_forgery
+		plug :put_secure_browser_headers
+	end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+	pipeline :api do
+		plug :accepts, ["json"]
+	end
 
-  scope "/", Hello do
-    pipe_through :browser # Use the default browser stack
+	scope "/", Hello do
+		pipe_through :browser	#	Use the default browser stack
 
-    get "/", PageController, :index
-  end
+		get "/hello", HelloController, :world
+		get "/", PageController, :index
+	end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Hello do
-  #   pipe_through :api
-  # end
+	#	Other scopes may use custom stacks.
+#	scope "/hello", HelloController do
+#		pipe_through :browser	#	Use the default browser stack
+#
+#		get "/hello", HelloController, :world
+#	end
 end
